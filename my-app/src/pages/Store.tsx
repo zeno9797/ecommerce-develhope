f5a95071f05b35a12c96be2f18fe9078522cba63
zeno <giuseppezeno97@gmail.com>
2023-07-09 15:46:50 +0200

    aggiunto carosello

--- my-app/src/pages/Store.tsx
+++ my-app/src/pages/Store.tsx
@@ -3,10 +3,23 @@ import storeItems from "../data/items.json"
 import { Row, Col} from "react-bootstrap"
 // COL sta per columns e ROW per riga
 import { StoreItem } from '../components/StoreItem'
+import Carousel from "react-bootstrap/Carousel"
 
 function Store() {
   return (
     <>
+    <Carousel>
+      <Carousel.Item>
+        <img src="http://localhost:3000/imgs/1.jpg" alt="" />
+        
+
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src="http://localhost:3000/imgs/2.jpg" alt="" />
+        
+
+      </Carousel.Item>
+    </Carousel>
     <div>Store</div>
     <Row md={2} xs={1} lg={3} className='g-3'>
       {storeItems.map(item => (
