10d7ee5eebb82e985e8b450bd4209a6f3b58bf5d
Giuseppe Oliveri <peppe7453@gmail.com>
2023-07-10 12:52:38 +0200

    added svg cart

--- my-app/src/components/Navbar.tsx
+++ my-app/src/components/Navbar.tsx
@@ -1,32 +1,51 @@
-import { Button, Nav, Container, Navbar as NavbarBs } from "react-bootstrap"
-import { NavLink } from "react-router-dom"
-import { useShoppingCart } from "../context/ShoppingCartContext"
+import { Button, Nav, Container, Navbar as NavbarBs } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import { useShoppingCart } from "../context/ShoppingCartContext";
 
 export function Navbar() {
-    const { openCart, cartQuantity } = useShoppingCart()
-    return (
-
-        <NavbarBs sticky="top" className="bg-white shadow-sm mb-4 pb-3">
-            <Container>
-                <Nav className="d-flex">
-                    <Nav.Link to="/" as={NavLink}>
-                        Logo
-                    </Nav.Link>
-                </Nav>
-                <div>
-                    <input type="text" />
-                    <button>cerca</button>
-                </div>
-                {cartQuantity > -1 && (
-                <Button onClick={openCart} style={{ width: "3rem", height: "3rem", position: "relative" }} variant="outline-primary" className="rounded-circle">
-                    SVG
-                    
-                    <div className="rounded-circle bg-danger d-flex justify-content-center align-items-center" style={{ color: "white", width: "1.5rem", height: "1.5rem", position: "absolute", bottom: 0, right: 0, transform: "translate(25%, 25%)" }}>
-                        {cartQuantity}
-                    </div>
-                </Button>)}
-
-            </Container>
-        </NavbarBs>
-    )
+  const { openCart, cartQuantity } = useShoppingCart();
+  return (
+    <NavbarBs sticky="top" className="bg-white shadow-sm mb-4 pb-3">
+      <Container>
+        <Nav className="d-flex">
+          <Nav.Link to="/" as={NavLink}>
+            Logo
+          </Nav.Link>
+        </Nav>
+        <div>
+          <input type="text" />
+          <button>cerca</button>
+        </div>
+        {cartQuantity > -1 && (
+          <Button
+            onClick={openCart}
+            style={{ width: "3rem", height: "3rem", position: "relative" }}
+            variant="outline-primary"
+            className="rounded-circle"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <g>
+                <path fill="none" d="M0 0h24v24H0z" />{" "}
+                <path d="M6 9h13.938l.5-2H8V5h13.72a1 1 0 0 1 .97 1.243l-2.5 10a1 1 0 0 1-.97.757H5a1 1 0 0 1-1-1V4H2V2h3a1 1 0 0 1 1 1v6zm0 14a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm12 0a2 2 0 1 1 0-4 2 2 0 0 1 0 4z" />{" "}
+              </g>
+            </svg>
+            <div
+              className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+              style={{
+                color: "white",
+                width: "1.5rem",
+                height: "1.5rem",
+                position: "absolute",
+                bottom: 0,
+                right: 0,
+                transform: "translate(25%, 25%)",
+              }}
+            >
+              {cartQuantity}
+            </div>
+          </Button>
+        )}
+      </Container>
+    </NavbarBs>
+  );
 }
