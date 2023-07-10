4df6be2bdc4e9c9b7725bff51ba2517afdc3e60c
Giuseppe Oliveri <peppe7453@gmail.com>
2023-07-10 13:44:44 +0200

    added hover effect in StoreItem

--- my-app/src/components/StoreItem.tsx
+++ my-app/src/components/StoreItem.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useShoppingCart } from "../context/ShoppingCartContext";
@@ -11,57 +12,66 @@ type StoreItemProps = {
 
 export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
   const { increaseCartQuantity } = useShoppingCart();
+  const [isHovered, setIsHovered] = useState(false);
   return (
     <>
-      <Card
-        className="w-100"
-        style={{
-          height: "420px",
-        }}
-      >
-        <Link to="/product">
+      <Link to="/product" style={{ textDecoration: "none" }}>
+        <Card
+          className="w-100"
+          style={{
+            height: "420px",
+            transition: "all 0.3s ease",
+            ...(isHovered && {
+              border: "3px #0d6efd solid",
+              boxShadow: "0px 5px 4px rgba(0, 0, 0, 0.2)",
+            }),
+          }}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
           <Card.Img
             variant="top"
             src={imgUrl}
             height="250px"
             style={{ objectFit: "cover" }}
           />
-        </Link>
-        <Card.Body className="d-flex flex-column">
-          <Card.Title
-            className="d-flex justify-content-between align-items-baseline mb-4"
-            style={{
-              flexDirection: "column",
-            }}
-          >
-            <span className="fs-2">{name}</span>
-            {/* fs = font size */}
-            <span className="ms-2 text-muted">{price} €</span>
-          </Card.Title>
-          <div className="mt-auto d-flex align-items-center flex-column">
-            <Button
-              onClick={() => increaseCartQuantity(id)}
-              className="w-100 "
-              style={{ display: "flex", justifyContent: "center", gap: 5 }}
+
+          <Card.Body className="d-flex flex-column">
+            <Card.Title
+              className="d-flex justify-content-between align-items-baseline mb-4"
+              style={{
+                flexDirection: "column",
+              }}
             >
-              <span>Add to cart</span>
-              <svg
-                style={{ color: "white", width: 25, height: 25 }}
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
+              <span className="fs-2">{name}</span>
+              {/* fs = font size */}
+              <span className="ms-2 text-muted">{price} €</span>
+            </Card.Title>
+            <div className="mt-auto d-flex align-items-center flex-column">
+              <Button
+                onClick={() => increaseCartQuantity(id)}
+                className="w-100 "
+                style={{ display: "flex", justifyContent: "center", gap: 5 }}
               >
-                <g>
-                  <path fill="none" d="M0 0h24v24H0z"></path>
-                  <path
-                    d="M6 9h13.938l.5-2H8V5h13.72a1 1 0 0 1 .97 1.243l-2.5 10a1 1 0 0 1-.97.757H5a1 1 0 0 1-1-1V4H2V2h3a1 1 0 0 1 1 1v6zm0 14a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm12 0a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"
-                    fill="white"
-                  ></path>
-                </g>
-              </svg>
-            </Button>
-          </div>
-        </Card.Body>
-      </Card>
+                <span>Add to cart</span>
+                <svg
+                  style={{ color: "white", width: 25, height: 25 }}
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <g>
+                    <path fill="none" d="M0 0h24v24H0z"></path>
+                    <path
+                      d="M6 9h13.938l.5-2H8V5h13.72a1 1 0 0 1 .97 1.243l-2.5 10a1 1 0 0 1-.97.757H5a1 1 0 0 1-1-1V4H2V2h3a1 1 0 0 1 1 1v6zm0 14a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm12 0a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"
+                      fill="white"
+                    ></path>
+                  </g>
+                </svg>
+              </Button>
+            </div>
+          </Card.Body>
+        </Card>
+      </Link>
     </>
   );
 }
