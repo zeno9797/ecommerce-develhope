cda2bed374f1eb3f40c5609adcdf4fefba010f78
Giuseppe Oliveri <peppe7453@gmail.com>
2023-07-12 17:19:09 +0200

    aaa

--- my-app/src/components/Navbar.tsx
+++ my-app/src/components/Navbar.tsx
@@ -6,7 +6,7 @@ import { Input } from "./Input";
 export function Navbar() {
   const { openCart, cartQuantity } = useShoppingCart();
   return (
-    <NavbarBs sticky="top" className="bg-white shadow-sm mb-4 pb-3">
+    <NavbarBs sticky="top" className="bg-danger shadow-sm mb-4 pb-3">
       <Container>
         <Nav className="d-flex">
           <Nav.Link to="/" as={NavLink}>
@@ -14,7 +14,7 @@ export function Navbar() {
           </Nav.Link>
         </Nav>
         <div>
-          <Input/>
+          <Input />
         </div>
         {cartQuantity > -1 && (
           <Button
