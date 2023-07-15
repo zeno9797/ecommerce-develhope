bb8458af1d4aaa17c02fe622b9d37d04da7b37f9
zeno <giuseppezeno97@gmail.com>
2023-07-15 21:28:34 +0200

    creato componente login

---
+++ my-app/src/pages/Login.tsx
@@ -0,0 +1,42 @@
+import { Button } from "react-bootstrap";
+import './productPage.css'
+
+
+export function Login() {
+    return (
+        <>
+
+            <form
+                className="form-user"
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: 15,
+                    
+                }}>
+                    <h3 style={{textAlign: "center"}}>Inserisci i tuoi dati</h3>
+                <input type="text"
+                    style={{
+                        padding: "6px",
+                        borderRadius: "5px",
+                        border: "1px solid black"
+                    }}
+                    placeholder="username" />
+                <input type="password"
+                    style={{
+                        padding: "6px", borderRadius: "5px",
+                        border: "1px solid black"
+                    }} placeholder="password" />
+                <Button
+
+                    style={{
+                        borderRadius: 5,
+                        border: '1px solid red',
+                    }}
+                    className="prd-btn">
+                    Submit
+                </Button>
+            </form>
+        </>
+    )
+}
