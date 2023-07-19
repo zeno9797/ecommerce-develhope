a2e91637114814cffed981b2f64eb418c44e048e
Giuseppe Oliveri <peppe7453@gmail.com>
2023-07-19 17:06:58 +0200

    server

--- server/index.js
+++ server/index.js
@@ -1,29 +1,29 @@
-const users = require("./data.json")
-console.log(users)
+const users = require("./data.json");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const port = 3001;
 
-const express = require('express')
-const app = express()
-const port = 3000
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.post('/login', (req, res) => {
-  const {username, password} = req.body
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
   const filterUser = users.filter((user) => {
-    return user.username === username && user.password === password
-  })
+    return user.username === username && user.password === password;
+  });
   if (filterUser.length > 0) {
-    res.status(200)
-    res.send("benveuto su game store")
+    res.status(200).send({ message: "Benvenuto su Game Store!" });
   } else {
-    res.status(402)
-    res.send("non sei autorizzato")
+    res.status(401).send({ error: "Non sei autorizzato." });
   }
-
-})
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
